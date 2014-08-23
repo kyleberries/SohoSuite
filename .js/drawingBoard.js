3fcf3717050ab737da31b759e08175466d965b4d
kyleberries <kyleberry.graphics@gmail.com>
2014-08-23 16:22:23 -0500

    fbcheck

--- .js/drawingBoard.js
+++ .js/drawingBoard.js
@@ -1,9 +1,6 @@
 /*
-if bad device alert('Unsupported device detected. All tools disabled')
-
-if ADB = red && FASTBOOT = red alert('No Device Detected')
-
 ALWAYS use fastboot cable unless directed otherwise (restore, possibly roms)
+
 
 ----------------------GUI----------------------------------------
 ADB  FASTBOOT                SohuSuite    alpha_ver0.5
@@ -11,6 +8,7 @@ ADB  FASTBOOT                SohuSuite    alpha_ver0.5
 {--------} >red or green, click to retest
 -----------------------------------------------------------------
 */
+
 //Global variables
 var terminal = require('shelljs');
 var Promise = require('bluebird');
@@ -18,53 +16,59 @@ var adbkit = require('adbkit');
 var client = adbkit.createClient();
 var adb;
 var fastboot;
+
 //if-else
-if(adb = 'true') $('.adbDot').css('color','green');
+if(adb = 'true') 
+     {
+	   $('.adbDot').css('color','green');
+	   $('#console').text('Device Detected');
+	 }
 if(adb = 'false') $('.adbDot').css('color','red');
-if(fastboot = 'true') $('.fastbootDot').css('color','green');
-if(fastboot = 'flase') $('.fastbootDot').css('color','red');
-if(fastboot = 'warning') $('.fastbootDot').css('color','yellow')
+if(fastboot = 'true') 
+     {
+	  $('.fastbootDot').css('color','green');
+      $('#console').text('Device Detected');
+	 }
+if(fastboot = 'false') $('.fastbootDot').css('color','red');
+if(fastboot = 'warning') $('.fastbootDot').css('color','yellow');
+
 //functions
+    
+	//Check for device in Fastboot and verify it is a KFSOWI
     function fastbootCheck(){
-        terminal.exec('fastboot -i 0x1949 devices', function(code, output) {
-            function(output){
-	            if(output = "") 
-			        {throw new Error('No Device Detected.');}
-				if(output.match(/DEVICE/g)) 
-				    {fastboot = 'warning';
-				      alert('unknown device detected');}
-				if(output != "")
-				    {terminal.exec('fastboot -i 0x1949 getvar [kindle]',function(code,     output){
-					         if(output == [kindle])
-						  		{fastboot = 'true'}
-						     if(output != [kindle])
-						        {throw new Error('Bad Device Detected.')}
+        terminal.exec('fastboot -i 0x1949 getvar product', function(output) {
+	            if(output.match(/Soho/g) != 'Soho') 
+			        {throw new Error('No fastboot kindle device detected.')}
+				if(output.match(/Soho/g = 'Soho')
+				    {terminal.exec('fastboot -i 0x1949 devices',function(output){
+							fbSerial = output.substr(0,16)
+							fastboot = 'true';
 						   })
-					   };
-                             }
-		.catch(function(err) 
-		        {alert(err);
-				 fastboot = 'false';
+					       };
+		                   });
+		.catch(function(err){
+		    alert(err);
+		    fastboot = 'false';
+			fbSerial = null;
                 });
-		});
 };		  
-		  
+		
+    //Check for booted device and verify it is a KFSOWI		
     function adbCheck(){
         client.listDevices()
         .then(function(devices) {
              if (devices.length <= 0) {
 			     $('.tool').css('display','block');
-			     throw new Error('No Device Detected.')};
+			     throw new Error('No ADB Device Detected.')};
                  return Promise.filter(devices, function(device) {
-		             $('.detector').css('text-shadow','1px 1px black');
                      return client.getProperties(device.id)
                      .then(function(properties) {
                          if(properties['ro.product.model'] != "KFSOWI" && properties['ro.product.model'] != "") {	
 					       $('.tool').css('display','none');
-		                   throw new Error('Wrong Device. KS WILL brick this device')}
-		   });
-       });
-  });
+		                   throw new Error('Wrong Device. SoSu WILL brick this device')}
+		                });
+                        });
+                        });
          .then(function(supportedDevices) {
 	        adb = 'true';
 	        $('.tool').css('display','block')})
