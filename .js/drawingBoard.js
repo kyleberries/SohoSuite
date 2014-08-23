311a7904e148de8cd24604baa7ca84a7c8816fbd
kyleberries <kyleberry.graphics@gmail.com>
2014-08-23 13:42:22 -0500

    dB testing

--- .js/drawingBoard.js
+++ .js/drawingBoard.js
@@ -1,3 +1,4 @@
+/*
 if bad device alert('Unsupported device detected. All tools disabled')
 
 if ADB = red && FASTBOOT = red alert('No Device Detected')
@@ -9,71 +10,65 @@ ADB  FASTBOOT                SohuSuite    alpha_ver0.5
  O      O <  id='adb/fastbootDot'
 {--------} >red or green, click to retest
 -----------------------------------------------------------------
-
+*/
+//Global variables
+var terminal = require('shelljs');
+var Promise = require('bluebird');
+var adbkit = require('adbkit');
+var client = adbkit.createClient();
 var adb;
 var fastboot;
-
-
-check connected device.id. (loop once for adb and fastboot?)
-if return does not contain "DEVICE"
-   check 'ro.product.model'
-    if 'ro.product.model' = 'KFSOWI'
-	   check 'ro.build.incremental' && var fireOS = incremental - extra text
-	      if fireOS != kernels[] $('#console').text('Error: FireOS version unsupported')
-		  else{  .catch(function(err) {$('#console').text('Error: ', err)})}
-		  
-		  
-
-		  
+//if-else
+if(adb = 'true') $('.adbDot').css('color','green');
+if(adb = 'false') $('.adbDot').css('color','red');
+if(fastboot = 'true') $('.fastbootDot').css('color','green');
+if(fastboot = 'flase') $('.fastbootDot').css('color','red');
+if(fastboot = 'warning') $('.fastbootDot').css('color','yellow')
+//functions
     function fastbootCheck(){
-       sudo.exec('fastboot -i 0x1949 devices', function(code, output) {
+        terminal.exec('fastboot -i 0x1949 devices', function(code, output) {
             function(output){
 	            if(output = "") 
 			        {throw new Error('No Device Detected.');}
 				if(output.match(/DEVICE/g)) 
-				    {$('#fastbootDot').css('color','yellow');
+				    {fastboot = 'warning';
 				      alert('unknown device detected');}
 				if(output != "")
-				    {sudo.exec('fastboot -i 0x1949 getvar [kindle]',function(code,     output){
-					      if(output == [kindle])
-						  		{$('#fastbootDot').css('color','green');}
-						  if(output != [kindle])
-						        {alert('Bad device detected! tools disabled');
-								  $('.tools').css('display','none')}
+				    {terminal.exec('fastboot -i 0x1949 getvar [kindle]',function(code,     output){
+					         if(output == [kindle])
+						  		{fastboot = 'true'}
+						     if(output != [kindle])
+						        {throw new Error('Bad Device Detected.')}
 						   })
 					   };
                              }
 		.catch(function(err) 
-		         {alert(err);
-				  $('#fastbootDot').css('color','red');
-                  })
+		        {alert(err);
+				 fastboot = 'false';
+                });
 		});
 };		  
-
 		  
-		  
-		  function adbCheck(){
-client.listDevices()
-  .then(function(devices) {
-   if (devices.length <= 0) {$('.tool').css('display','block'); throw new Error('No Device Detected.')};
-    return Promise.filter(devices, function(device) {
-		$('.detector').css('text-shadow','1px 1px black');
-      return client.getProperties(device.id)
-        .then(function(properties) {
-          if(properties['ro.product.model'] != "KFSOWI" && properties['ro.product.model'] != "") {	$('.tool').css('display','none');
-		  throw new Error('Wrong Device. KS WILL brick this device');
-		  $('#adbDot').css('color','red');
-        }})
-    })
-  })
-  .then(function(supportedDevices) {
-    $('#detector').text('KFSOWI detected: '+ supportedDevices
-	adb = true;
-	$('.tool').css('display','block');
-	$('#adbDot').css('color','green');
-  })
-  .catch(function(err) {
-    alert(err);
-		$('#adbDot').css('color','red');
-  })
+    function adbCheck(){
+        client.listDevices()
+        .then(function(devices) {
+             if (devices.length <= 0) {
+			     $('.tool').css('display','block');
+			     throw new Error('No Device Detected.')};
+                 return Promise.filter(devices, function(device) {
+		             $('.detector').css('text-shadow','1px 1px black');
+                     return client.getProperties(device.id)
+                     .then(function(properties) {
+                         if(properties['ro.product.model'] != "KFSOWI" && properties['ro.product.model'] != "") {	
+					       $('.tool').css('display','none');
+		                   throw new Error('Wrong Device. KS WILL brick this device')}
+		   });
+       });
+  });
+         .then(function(supportedDevices) {
+	        adb = 'true';
+	        $('.tool').css('display','block')})
+        .catch(function(err) {
+             alert(err);
+		     adb = 'false'})
   };
