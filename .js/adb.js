f4c29cfd07f7de019466ba4f5676bb2265e381ba
kyleberries <kyleberry.graphics@gmail.com>
2014-08-25 11:49:26 -0500

    custom frame allows adb kill onclose

--- .js/adb.js
+++ .js/adb.js
@@ -1,6 +1,18 @@
+var nw = require('nw.gui');
+var win = nw.Window.get();
+win.isMaximized = false;
 var fbSerial = null;
 var adbSerial = null;
 var exec = require('child_process').exec;
+
+function shutdown(){win.close(killAdb())};
+function mini(){win.minimize()};
+function maxi(){
+            if (win.isMaximized)
+                win.unmaximize();
+            else
+                win.maximize();
+        };
 function cmd(command, callback) {
     var proc = exec(command);
 
@@ -22,6 +34,12 @@ function clearCache(){
 sudo.rm('-rf', './.js/cache/*');
 console('Cache cleared.');
 }; 
+function startAdb(){
+    cmd('adb devices',function(){})
+}
+function killAdb(){
+    cmd('adb kill-server',function(){})
+}
 function adbPush(local,kindle){
    if(adbSerial !== null){cmd('adb push -s '+adbSerial+' '+local+' '+kindle,function(stdout){console(stdout+' complete')})}
    else if(adbSerial == null) throw new Error('No device detected [adbPush]')
@@ -37,21 +55,21 @@ function adbShell(command){
    else if(adbSerial==null)throw new Error('No device detected [adbShell]')
    else {throw new Error('command fail, or detection fail')}
   }; 
-function fbFlash(kernel){
- //cmd('fastboot -s '+fbSerial+' boot '+kernel, function(stdout){}
-};
-function fastbootCheck(){
-    cmd('fastboot -i 0x1949 devices',function(stdout){
-        if(stdout.length<15) {throw Error('No device detected [fb] ',002);$('.fastboot').css('color','red')}
-        else if(stdout.length>15) {console(stdout);$('.fastboot').css('color','green')}
-})
-};
 function adbCheck(){
     cmd('adb devices',function(stdout){
         if(stdout.length<30) {throw Error('No device detected [adb]',001);$('.adb').css('color','red')}
         else if(stdout.length>30) {console(stdout);$('.adb').css('color','green')}
 })
 }
+function fastbootCheck(){
+    cmd('fastboot -i 0x1949 devices',function(stdout){
+        if(stdout.length<15) {throw Error('No device detected [fb] ',002);$('.fastboot').css('color','red')}
+        else if(stdout.length>15) {console(stdout);$('.fastboot').css('color','green')}
+})
+};
+function fbFlash(kernel){
+ //cmd('fastboot -s '+fbSerial+' boot '+kernel, function(stdout){}
+};
 function root(){
 console('ROOT coming soon...');
 /*
@@ -71,17 +89,14 @@ console('HELLFIRE coming soon...')
 function plasmaInstall(){
 console('PLASMA coming soon...')
 };
-function startAdb(){
-    cmd('adb devices',function(){})
-}
 
 
-
-
-
-
-
-
-process.on('uncaughtException', function (exception) {
+	        win.on('maximize', function(){
+            win.isMaximized = true;
+        });
+            win.on('unmaximize', function(){
+            win.isMaximized = false;
+        });
+            process.on('uncaughtException', function (exception) {
    console(exception)
   }); 
