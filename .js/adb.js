575e9ca114c83eb0d09aa5561569fe065c6a84f2
kyleberries <kyleberry.graphics@gmail.com>
2014-08-24 19:37:35 -0500

    Detector

--- .js/adb.js
+++ .js/adb.js
@@ -8,43 +8,42 @@ function console(output){
  }
 function clearCache(){
 sudo.rm('-rf', './.js/cache/*');
-$('#console').text('Cache cleared.');
+console('Cache cleared.');
 }; 
 function adbPush(local,kindle){
    if(adbSerial !== null){cmd('adb push -s '+adbSerial+' '+local+' '+kindle,function(stdout){console(stdout)})}
-   else {throw new Error('xfer fail, or detection fail')}
+   else if(adbSerial == null) throw new Error('No device detected [adbPush]')
+   else {throw new Error('xfer fail.')}
 };
 function adbPull(kindle,local){
    if(adbSerial !== null){cmd('adb pull -s '+adbSerial+' '+kindle+' '+local,function(stdout){console(stdout)})}
+   else if(adbSerial==null)throw new Error('No device detected [adbPull]')
    else {throw new Error('xfer fail, or detection fail')}
 };
 function adbShell(command){
     if(adbSerial !== null){cmd('adb shell -s '+command,function(stdout){console(stdout)})}
+   else if(adbSerial==null)throw new Error('No device detected [adbShell]')
    else {throw new Error('command fail, or detection fail')}
   }; 
 function fastbootCheck(){
  cmd('fastboot -i 0x1949 devices',function(stdout){
-if(stdout == null) throw Error('No device detected');
+if(stdout == null) throw Error('No device detected',002);
 else if(stdout !== null){fbSerial = stdout.substr(0,16);cmd('fastboot -i 0x1949 getvar product',function(stdout){
                                          if(stdout.match(/Soho/g) !== 'Soho'){fbSerial = null}})}
   })
+  if(fbSerial!==null)$('.fastboot').css('color','green');
+else if(fbSerial==null)$('.fastboot').css('color','red');
 };
 function adbCheck(){
     cmd('adb devices',function(stdout){
-        if(stdout == null) throw Error('No device detected');
+        if(stdout == null) throw Error('No device detected',001);
         else if(stdout !== null){ adbSerial = stdout.substr(0,16);cmd('adb shell getprop ro.product.model', function(stdout){
 		                                 if(stdout != 'KFSOWI'){adbSerial = null}})}
 	  })
-};
-function detector(){
-fastbootCheck();
-if(fbSerial!==null)$('.fastboot').css('color','green');
-else if(fbSerial==null)$('.fastboot').css('color','red');
-adbCheck();
-if(adbSerial!==null)$('.adb').css('color','green');
+	  if(adbSerial!==null)$('.adb').css('color','green');
 else if(adbSerial==null)$('.adb').css('color','red');
 };
 
 process.on('uncaughtException', function (exception) {
-   console(exception.message)
+   console(exception)
   }); 
