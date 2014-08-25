cadf2a92566c8ba4e915f1480ef4bb44916d7778
kyleberries <kyleberry.graphics@gmail.com>
2014-08-25 16:29:07 -0500

    pages upgrade

--- .js/jquery.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function(){
-  $('#adbInstall').click(function(){window.open('bin/adbInstaller.exe')});
-  $('.romLink').click(function(){$('.toggle').toggle('slow')});
-  $(function(){$('.scroll').slimScroll({height: '320px'});});
-});
