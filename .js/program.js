c4f84f6dfcc2403d0a9baea169926e0aa42c42c6
kyleberries <kyleberry.graphics@gmail.com>
2014-09-10 13:01:07 -0500

    full function test

--- .js/program.js
+++ .js/program.js
@@ -1,11 +1,13 @@
 var adb = require('adbkit');
+var fs = require('fs')
 var Promise = require('bluebird');
 var client = adb.createClient();
 var fbSerial;
+var dev = null;
 var markdown = require( "markdown" ).markdown;
 var exec = require('child_process').exec;
-function console(stdout){
-$('#console').text(stdout);
+function console(output){
+$('#console').text(output);
 }
 function cmd(command, callback) {
     var proc = exec(command);
@@ -29,10 +31,7 @@ setTimeout(
 		 else{fbSerial==false;throw Error('fastboot >No device detected')}
 	}),1000)
 };
-function fbFlash(kernel){
- //cmd('fastboot -s '+fbSerial+' boot '+kernel, function(stdout){}
-};
-
+//complete
 function adbCheck(){
 client.listDevices()
   .then(function(devices) {
@@ -40,21 +39,56 @@ client.listDevices()
     return Promise.filter(devices, function(device) {
       return client.getProperties(device.id)
         .then(function(properties) {
+		 	 dev = device.id;
           var model = properties['ro.product.model'];
 		  if(model=='KFSOWI' || model=='sdk'){console('adb >KFSOWI detected.')
 		                                      $('#console').css('color','green')}
-		  else {throw Error('adb >Unsupported device.')}
+		  else {dev=null;throw Error('adb >Unsupported device.')}
         })
     })
   }
-  else throw Error('adb >No device detected');
+  else {dev=null;throw Error('adb >No device detected')}
   })
   .catch(function(err) {
     console(err)
   })
 }
-
-//ERROR Handler
+function adbShell(command){
+if(dev==null){throw Error('adb >No KFSOWI detected')};
+client.shell(dev,command)
+ .then(console('adb >'+command+' complete'))
+};
+function adbPush(local,remote){
+if(dev == null){throw Error('No KFSOWI detected')}
+client.listDevices()
+  .then(function(devices) {
+    return Promise.map(devices, function(device) {
+      return client.push(device.id,local,remote)
+        .then(function(transfer) {
+          return new Promise(function(resolve, reject) {
+            transfer.on('progress', function(stats) {
+              console('[%s] Pushed %d bytes so far',
+                device.id,
+                stats.bytesTransferred)
+            })
+            transfer.on('end', function() {
+              console('[%s] Push complete', device.id)
+              resolve()
+            })
+            transfer.on('error', reject)
+          })
+        })
+    })
+  })
+  .then(function() {
+    console('Done pushing '+local+' to '+remote)
+  })
+  .catch(function(err) {
+    connsole(err.stack)
+  })
+};
+  
+  //ERROR Handler
 		   process.on('uncaughtException', function (exception) {
    $('#console').css('color','red');
    $('#console').text(exception);
