8f67c1acabdf19325b881b3d93d0efa93575744f
kyleberries <kyleberry.graphics@gmail.com>
2014-09-13 16:42:38 -0500

    test root/restore 1.2

---
+++ .js/restore.js
@@ -0,0 +1,35 @@
+//flash 11310 kernel/recovery and minisystem
+function restore1(){
+alert('Please power Kindle off, and plug into fastboot cable');
+if(fbSerial==null){throw Error('fastboot >No device detected');
+                    $('#restore').attr("onclick","restore1()").attr("value","Restore");}
+fastboot('flash boot ./.resources/11310/boot.img');
+fastboot('flash recovery ./.resources/11310/recovery.img');
+fastboot('flash system ./.resources/restore/minisys.img');
+fastboot('continue');
+console('Please wait until device reconnects in adb mode above before continuing.')
+$('#restore').attr("onclick","restore2()").attr("value","Next");
+console('Verify Kindle is in fastboot mode, and the fastboot indicator above is green ,then click next.');
+};
+
+//run wipe, and restore
+function restore2(){
+if(adbSerial==null){throw Error('fastboot >No device detected');
+                    $('#restore').attr("onclick","restore1()").attr("value","Restore");}
+adbShell('su -c mount -o remount rw, /data');
+adbShell('su -c rm -rf /data/*');
+adbShell('su -c chmod 777 /cache');
+adbShell('su -c mount -o remount rw, /cache')
+adbShell('mkdir /cache/recovery');
+adbPush('./.resources/restore/update.zip','/cache/update.zip');
+adbShell('su -c echo --update_package=/cache/update.zip>/cache/recovery/command')
+console('Please unplug Kindle, and plug in with USB cable. When device reconnects click next.');
+$('#restore').attr("onclick","restore3()");
+};
+
+//execute restore
+function restore3(){
+adbShell('su -c reboot recovery');
+console('Kindle will display upgrade screen, and restore to stock.');
+$('#restore').attr("onclick","restore1()").attr("value","Restore");
+};
