1fd662fb431a0194a609d2d917640ebf1ae835b4
kyleberries <kyleberry.graphics@gmail.com>
2014-09-16 17:18:29 -0500

    Revert "test"
    This reverts commit 628b07c6b42df363c7684e4727c8a8a67c078789.

--- .js/restore.js
+++ .js/restore.js
@@ -1,6 +1,6 @@
 function restore1(){
 alert('Please power Kindle off, and plug into fastboot cable');
-if(fbSerial==null){$('#restore').attr("onclick","restore1()").attr("value","Restore");                   throw Error('fastboot >No Kindle detected')}
+if(fbSerial==null){$('#restore').attr("onclick","restore1()").attr("value","Restore");                   throw Error('fastboot No Kindle detected')}
 fastboot('getvar all');
 fastboot('getvar product');
 $('#restore').attr("onclick","restore2()").attr("value","Next");
