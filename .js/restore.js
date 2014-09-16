8a75623a98c6ed91390576a065db3533832e521c
kyleberries <kyleberry.graphics@gmail.com>
2014-09-16 14:46:55 -0500

    test

--- .js/restore.js
+++ .js/restore.js
@@ -1,8 +1,8 @@
 function restore1(){
 alert('Please power Kindle off, and plug into fastboot cable');
 if(fbSerial==null){$('#restore').attr("onclick","restore1()").attr("value","Restore");                   throw Error('fastboot No Kindle detected')}
-fastboot('getvar product');
 fastboot('getvar all');
+fastboot('getvar product');
 $('#restore').attr("onclick","restore2()").attr("value","Next");
 alert('click next to reboot to home screen');
 };
