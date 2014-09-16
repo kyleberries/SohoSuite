960ab2416c29904bad0b92c5a503dbff85b16357
kyleberries <kyleberry.graphics@gmail.com>
2014-09-16 12:55:46 -0500

    test

--- .js/restore.js
+++ .js/restore.js
@@ -4,12 +4,15 @@ alert('Please power Kindle off, and plug into fastboot cable');
 fbCheck();
 if(fbSerial==null){$('#restore').attr("onclick","restore1()").attr("value","Restore");}
 fastboot('getvar product');
-fastboot('continue');
+$('#restore').attr("onclick","restore2()").attr("value","Next");
+alert('click next to reboot to home screen');
 };
 
-/*run wipe, and restore
+//run wipe, and restore
 function restore2(){
-if(adbSerial==null){throw Error('fastboot >No device detected');
+fastboot('continue');
+$('#restore').attr("onclick","restore1()").attr("value","Restore");
+/*if(adbSerial==null){throw Error('fastboot >No device detected');
                     $('#restore').attr("onclick","restore1()").attr("value","Restore");}
 adbShell('su -c mount -o remount rw, /data');
 adbShell('su -c rm -rf /data/*');
@@ -20,9 +23,10 @@ adbPush('./.resources/restore/update.zip','/cache/update.zip');
 adbShell('su -c echo --update_package=/cache/update.zip>/cache/recovery/command')
 console('Please unplug Kindle, and plug in with USB cable. When device reconnects click next.');
 $('#restore').attr("onclick","restore3()");
+*/
 };
 
-//execute restore
+/*execute restore
 function restore3(){
 adbShell('su -c reboot recovery');
 console('Kindle will display upgrade screen, and restore to stock.');
